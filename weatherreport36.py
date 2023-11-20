52cbca7e2a21a7b6cc253cc536facf77ea1e85a5
Ian Fu  (It-àn Pòo) <peaceian9527@gmail.com>
2023-11-20 23:39:48 +0800

    Update weatherreport36.py

--- weatherreport36.py
+++ weatherreport36.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import json
 from json import loads
 # Create your views here.
-user_key = 'CWB-869C7631-CE97-4263-9AF8-635DF71E8A67'
+user_key = 'CWB-869C7631-CE97-4263-8AF8-635DF71E8A67'
 doc_name = 'F-C0032-001'
 
 url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/%s?Authorization=%s" % (doc_name,user_key)
